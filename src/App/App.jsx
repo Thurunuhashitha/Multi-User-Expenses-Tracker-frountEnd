12d666c09828c34a8eef1c63fac35252b0535e09
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-01-30 09:14:02 +0530

    developed register page and link to navigate

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,18 @@
 import Login from '../Pages/Login/Login'
+import Register from '../Pages/Register/Register'
 import './App.css'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 function App() { 
 
   return (
-    <>
-       <Login/>
-    </>
+    <div>  
+      <Routes>   
+        <Route path='/login' element = {<Login/>} />         {/* url ekee "/login" meeka gahuwoth ookata yanawa */}
+        <Route path='/register' element = {<Register/>} /> 
+      </Routes>
+    </div>
   )
 }
 
