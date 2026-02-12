3b9269c808fc2abb143405ce170e9a09c62f1a06
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-02-12 13:39:09 +0530

    bug fix

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -11,8 +11,8 @@ function App() {
     <div>  
       <Routes>   
         {/* Default route */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        
+        <Route path="/" element={<Register/>} />
+
         <Route path='/login' element = {<Login/>} />         {/* url ekee "/login" meeka gahuwoth ookata yanawa */}
         <Route path='/register' element = {<Register/>} /> 
         <Route path='/expenses' element = {<Expenses/>} /> 
